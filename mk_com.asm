c0ecd9080590611d58b4cfa8f7a4849f322109ed
Šarūnas Sinkevičius <sinkevicius.sarunas3@gmail.com>
2025-12-12 02:39:26 +0200

    Added a testing file.

--- mk_com.asm
+++ mk_com.asm
@@ -1,12 +1,41 @@
 .model small
 
 .code
-
 org 100h
 
 start:
 
-    mov ax, 09h
-		mov dx, 01h
+    mov ax, bx
+    mov cx, dx
+
+    nop
+
+    mov ax, [bx]
+    mov dx, [bp+di+20h]
+    mov [bx], ax
+    mov [bp+di], dx
+
+    nop
+
+    mov es, ax
+    mov ax, es
+
+    nop
+
+    mov es, word ptr [bx]
+    mov word ptr [di+6], ds
+
+    nop
+
+    mov ds:[1000h], ax
+
+    nop
+
+    mov al, 42h
+    mov word ptr [bx+di+100h], 0CDEFh
+    mov ax, [bp+1234h]
+    
+    mov ah, 4Ch
+    int 21h
 
 end start
